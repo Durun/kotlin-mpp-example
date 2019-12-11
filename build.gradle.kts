bb04eaea9514deddcb82cc86b7264be7d8ca5d27
Durun <negiigainuki@gmail.com>
2019-12-11 14:29:22 +0900

    setup gradle

---
+++ build.gradle.kts
@@ -0,0 +1,61 @@
+
+plugins {
+    /**
+     * Check:
+     *  - Is the gradle not "offline-mode" ?
+     *  - Is the JVM version in which the gradle runs correct?
+     */
+    kotlin("multiplatform") version "1.3.61"
+}
+
+group = "io.github.durun"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+kotlin {
+    sourceSets {
+        commonMain {
+            dependencies {
+                implementation(kotlin("stdlib-common"))
+            }
+        }
+        commonTest {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+    }
+
+    linuxX64 {
+        /**
+         * Required files:
+         *  /usr/lib/libtinfo.so.5
+         *      arch-linux -> install "ncurses5-compat-libs" from AUR
+         */
+        binaries {
+            executable()
+        }
+    }
+
+    macosX64 {
+        binaries {
+            executable()
+        }
+    }
+
+    mingwX64(name = "windows") {
+        binaries {
+            executable()
+        }
+    }
+}
+
+tasks.withType<Wrapper> {
+    gradleVersion = "5.3.1"
+    distributionType = Wrapper.DistributionType.ALL
+}
